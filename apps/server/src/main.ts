8e995d4aebd4f0e35ea0886ab7ea9d77f359999d
SoHeil Vatanpoor <soheilsobeck@gmail.com>
2024-10-12 19:40:16 +0330

    fix: server port

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from "@nestjs/core";
+import isNaN from "lodash/isNaN";
 
 import { AppModule } from "~/modules/app/app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+
+	await app.listen(getPort());
 }
+
+const getPort = () => {
+	const PORT = Number(process.env.PORT);
+
+	if (isNaN(PORT)) throw Error("UNKNOWN_PORT");
+
+	return PORT;
+};
 
 bootstrap();
