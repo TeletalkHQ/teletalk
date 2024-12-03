a5c3296881677d7791e185d8e57f6988272ab3c1
SoHeil Vatanpoor <soheilsobeck@gmail.com>
2024-12-03 12:21:13 +0330

    cleanup providers

--- packages/ui/src/providers/theme.tsx
+++ packages/ui/src/providers/theme.tsx
@@ -1,10 +1,10 @@
-import { CssBaseline, GlobalStyles } from "@mui/material";
+import { CssBaseline } from "@mui/material";
 import { Theme, ThemeProvider as ThemeProviderMui } from "@mui/material";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import { BaseSchema } from "@repo/schema";
 import React, { FC, PropsWithChildren } from "react";
 
 import { useThemeState } from "../hooks";
-import { EmotionCacheProvider } from "./emotionCache";
 
 export type Themes = Record<BaseSchema.ThemeName, Theme>;
 
@@ -18,7 +18,6 @@ interface Props extends PropsWithChildren {
 }
 
 export const ThemeProvider: FC<Props> = ({
-	appName,
 	children,
 	forceThemeName,
 	shouldUseBaseline = true,
@@ -28,59 +27,12 @@ export const ThemeProvider: FC<Props> = ({
 
 	const selectedTheme = themes[forceThemeName || theme];
 
-	// TODO: Remove me pls :)
-	const color: Record<AppName, string> = {
-		admin: selectedTheme.palette.secondary[600],
-		map: selectedTheme.palette.secondary[600],
-		dashboard: selectedTheme.palette.secondary[600],
-		health: selectedTheme.palette.secondary[600],
-	};
-
-	const autofillColor = color[appName];
-
 	return (
-		<EmotionCacheProvider options={{ key: "mui" }}>
+		<AppRouterCacheProvider options={{ enableCssLayer: true }}>
 			<ThemeProviderMui theme={selectedTheme}>
 				{shouldUseBaseline && <CssBaseline />}
 				{children}
 			</ThemeProviderMui>
-
-			<GlobalStyles
-				styles={{
-					"@-webkit-keyframes autofill": {
-						from: {
-							backgroundColor: `${autofillColor}`,
-						},
-					},
-					"@-webkit-keyframes autofill-focus": {
-						from: {
-							backgroundColor: `${autofillColor}`,
-						},
-					},
-					"@-webkit-keyframes autofill-hover": {
-						from: {
-							backgroundColor: `${autofillColor}`,
-						},
-					},
-					"@-webkit-keyframes autofill-active": {
-						from: {
-							backgroundColor: `${autofillColor}`,
-						},
-					},
-					"input:-webkit-autofill": {
-						WebkitBoxShadow: `0 0 0 30px ${autofillColor} inset !important`,
-					},
-					"input:-webkit-autofill:hover": {
-						WebkitBoxShadow: `0 0 0 30px ${autofillColor} inset !important`,
-					},
-					"input:-webkit-autofill:focus": {
-						WebkitBoxShadow: `0 0 0 30px ${autofillColor} inset !important`,
-					},
-					"input:-webkit-autofill:active": {
-						WebkitBoxShadow: `0 0 0 30px ${autofillColor} inset !important`,
-					},
-				}}
-			/>
-		</EmotionCacheProvider>
+		</AppRouterCacheProvider>
 	);
 };
